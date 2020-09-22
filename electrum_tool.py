2d362a5f6425b1eafad2c9d759189b8ab9cc9d70
nR2Cncdm <7zzasd0598x@tutanota.com>
2020-09-22 12:55:25 -0400

    First Commit!

---
+++ electrum_tool.py
@@ -0,0 +1,200 @@
+# Copyright (c) 2009 Bitcoin Company
+# Author: Satoshi Nakamoto
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# This example is for use on (Linux) computers that are using CPython with
+# Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+# not support PIL/pillow (python imaging library)!
+
+import time
+# import random
+from colorsys import hsv_to_rgb
+import subprocess
+import board
+from digitalio import DigitalInOut, Direction
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+
+# Create the display
+cs_pin = DigitalInOut(board.CE0)
+dc_pin = DigitalInOut(board.D25)
+reset_pin = DigitalInOut(board.D24)
+BAUDRATE = 24000000
+
+spi = board.SPI()
+disp = st7789.ST7789(
+    spi,
+    height=240,
+    y_offset=80,
+    rotation=180,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+)
+
+# Input pins:
+button_A = DigitalInOut(board.D5)
+button_A.direction = Direction.INPUT
+
+button_B = DigitalInOut(board.D6)
+button_B.direction = Direction.INPUT
+
+button_L = DigitalInOut(board.D27)
+button_L.direction = Direction.INPUT
+
+button_R = DigitalInOut(board.D23)
+button_R.direction = Direction.INPUT
+
+button_U = DigitalInOut(board.D17)
+button_U.direction = Direction.INPUT
+
+button_D = DigitalInOut(board.D22)
+button_D.direction = Direction.INPUT
+
+button_C = DigitalInOut(board.D4)
+button_C.direction = Direction.INPUT
+
+# Turn on the Backlight
+backlight = DigitalInOut(board.D26)
+backlight.switch_to_output()
+backlight.value = True
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for color.
+width = disp.width
+height = disp.height
+image = Image.new("RGB", (width, height))
+
+# First define some constants to allow easy resizing of shapes.
+padding = 122
+top = padding
+bottom = height - padding
+
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 10
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Clear display.
+draw.rectangle((0, 0, width, height), outline=0, fill=(255, 0, 0))
+disp.image(image)
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+udlr_fill = "#00FF00"
+udlr_outline = "#00FFFF"
+button_fill = "#FF00FF"
+button_outline = "#FFFFFF"
+
+fnt = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 16)
+
+while True:
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    if not button_U.value:  # up pressed
+        cmd = "hostname -I | cut -d' ' -f1"
+        IP = "IP: " + subprocess.check_output(cmd, shell=True).decode("utf-8")
+    else:
+        IP = "" # up
+
+    if not button_D.value:  # down pressed
+        cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+        CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    else:
+        CPU = ""  # down
+
+    if not button_L.value:  # left pressed
+        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+        MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    else:
+        MemUsage = ""  # left
+
+    if not button_R.value:  # right pressed
+        cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+        Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    else:
+        Disk = ""  # right
+
+    if not button_C.value:  # center pressed
+        cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}'"  # pylint: disable=line-too-long
+        Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    else:
+        Temp = ""  # center
+
+    A_fill = 0
+    if not button_A.value:  # left pressed
+        A_fill = button_fill
+    draw.ellipse((140, 80, 180, 120), outline=button_outline, fill=A_fill)  # A button
+
+    B_fill = 0
+    if not button_B.value:  # left pressed
+        B_fill = button_fill
+    draw.ellipse((190, 40, 230, 80), outline=button_outline, fill=B_fill)  # B button
+
+    # make a random color and print text
+    # rcolor = tuple([int(x * 255) for x in hsv_to_rgb(random.random(), 1, 1)])
+    # draw.text((20, 150), "Hello World", font=fnt, fill=rcolor)
+    # rcolor = tuple([int(x * 255) for x in hsv_to_rgb(random.random(), 1, 1)])
+    # draw.text((20, 180), "Hello World", font=fnt, fill=rcolor)
+    # rcolor = tuple([int(x * 255) for x in hsv_to_rgb(random.random(), 1, 1)])
+    # draw.text((20, 210), "Hello World", font=fnt, fill=rcolor)
+
+    # Shell scripts for system monitoring from here:
+    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    # cmd = "hostname -I | cut -d' ' -f1"
+    # IP = "IP: " + subprocess.check_output(cmd, shell=True).decode("utf-8")
+    # cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    # CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    # cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+    # MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    # cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+    # Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    # cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}'"  # pylint: disable=line-too-long
+    # Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+    # Write four lines of text.
+    y = top
+    draw.text((x, y), IP, font=font, fill="#FFFFFF")
+    y += font.getsize(IP)[1]
+    draw.text((x, y), CPU, font=font, fill="#FFFF00")
+    y += font.getsize(CPU)[1]
+    draw.text((x, y), MemUsage, font=font, fill="#00FF00")
+    y += font.getsize(MemUsage)[1]
+    draw.text((x, y), Disk, font=font, fill="#0000FF")
+    y += font.getsize(Disk)[1]
+    draw.text((x, y), Temp, font=font, fill="#FF00FF")
+
+    # Display the Image
+    disp.image(image)
+
+    time.sleep(0.01)
