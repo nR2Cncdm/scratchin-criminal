b95ea25f4ae0f00891e6e3b3109a8c73a73ba043
nR2Cncdm <7zzasd0598x@tutanota.com>
2020-10-04 10:59:49 -0400

    fix

--- electrum_tool.py
+++ electrum_tool.py
@@ -153,22 +153,8 @@ while True:
     if not button_C.value:  # center pressed
         #Show QR Code for Test Address
         image = canvas
-        # image = Image.open('myqr.jpg')
-        # # Scale the image to the smaller screen dimension
-        # image_ratio = image.width / image.height
-        # screen_ratio = width / height
-        # if screen_ratio < image_ratio:
-            # scaled_width = image.width * height // image.height
-            # scaled_height = height
-        # else:
-            # scaled_width = width
-            # scaled_height = image.height * width // image.width
-        # image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
-
-        # # Crop and center the image
-        # x = scaled_width // 2 - width // 2
-        # y = scaled_height // 2 - height // 2
-        # image = image.crop((x, y, x + width, y + height))
+    else:
+		image = Image.new("RGB", (width, height))  
 
     A_fill = 0
     if not button_A.value:  # left pressed
